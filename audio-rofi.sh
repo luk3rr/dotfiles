47d647dde9aafb6479a532c0cb34764e7bb39fc1
luk3rr <luk3rr@gmail.com>
2023-01-10 15:20:47 -0300

    show intuitive options in menu

--- audio-rofi.sh
+++ audio-rofi.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env sh
 
 get_sinks() {
-    availableSinks=$(pacmd list-sinks | grep -e 'name:' | cut -d ':' -f 2)
+    availableSinks=$(pacmd list-sinks | grep -e 'name:' | cut -d ' ' -f 2 | sed 's/<//g' | sed 's/>//g')
+    sinksNames=$(pacmd list-sinks | grep -e 'bluez.alias' -e 'alsa.card_name' | cut -d '=' -f 2 | sed 's/"//g')
     lineNum=$(echo "$availableSinks" | wc -l)
 }
 
 menu() {
     get_sinks
-    chosen=$(echo -e "$availableSinks" | uniq -u | rofi -dmenu -p "SINK: " -lines "$lineNum")
+
+    #-format 'i' to return index
+    chosen=$(echo -e "$sinksNames" | rofi -dmenu -format 'i' -p "SINK: " -lines "$lineNum")
 
     if [[ "$chosen" == "" ]]; then
         return 0
 
     else
-        # Cut '< >' of chosen name
-        chosen="${chosen:2:-1}"
-        pacmd set-default-sink "$chosen"
+        declare -a availabre=($availableSinks)
+        pacmd set-default-sink "${availabre[$chosen]}"
     fi
+
     defaultSink=$(pacmd stat | grep -e 'Default sink name:' | cut -d ':' -f 2)
     echo "Current default sink: $defaultSink 👺"
 }
