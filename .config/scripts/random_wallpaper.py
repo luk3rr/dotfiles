660372cc1471c322fff0c162bc5c8be900aa0e76
luk3rr <luk3rr@gmail.com>
2023-01-20 12:34:03 -0300

    change messages and variable names

--- .config/scripts/random_wallpaper.py
+++ .config/scripts/random_wallpaper.py
@@ -18,12 +18,14 @@ RATIO_TOLERANCE = 0.3
 
 def make_list(wallpapers_dir, list_path, screen_width, screen_height, screen_ratio):
     files = [f for f in scan(wallpapers_dir + "**/*.*", recursive=True)]
-    print(">> {} files located.".format(len(files)), end=" ")
+
+    print(">> {} files found.".format(len(files)), end=" ")
     print("\n>> Loading...", end=" ")
-    start_Time = time()
+
+    start_time = time()
 
     try:
-        wallpaper_list = open(list_path + "/screen-list_" + str(screen_width) + "x" + str(screen_height) + ".txt", 'w+')
+        wallpaper_list = open(list_path + "/.wallpaper-list_" + str(screen_width) + "x" + str(screen_height) + ".txt", 'w+')
 
     except IOError:
         print(">> Could not read file")
@@ -41,16 +43,16 @@ def make_list(wallpapers_dir, list_path, screen_width, screen_height, screen_rat
                 wallpaper_list.write(f + '\n')
 
     wallpaper_list.close()
-    print("\n>> End function.\n>> Duraction: {}".format(time() - start_Time))
+    print("\n>> End function.\n>> Elapsed time: {}".format(time() - start_time))
 
 def select_wallpaper(wallpapers_dir, list_path, screen_width, screen_height, screen_ratio):
     try:
-        wallpaper_list = open(list_path + "/screen-list_" + str(screen_width) + "x" + str(screen_height) + ".txt").read().splitlines()
+        wallpaper_list = open(list_path + "/.wallpaper-list_" + str(screen_width) + "x" + str(screen_height) + ".txt").read().splitlines()
 
     except IOError:
         print(">> List do not exists. Creating...\n")
         make_list(wallpapers_dir, list_path, screen_width, screen_height, screen_ratio)
-        wallpaper_list = open(list_path + "/screen-list_" + str(screen_width) + "x" + str(screen_height) + ".txt").read().splitlines()
+        wallpaper_list = open(list_path + "/.wallpaper-list_" + str(screen_width) + "x" + str(screen_height) + ".txt").read().splitlines()
 
     line = choice(wallpaper_list)
     system("feh --bg-fill " + line)
