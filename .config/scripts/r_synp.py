d305d94f250e94ead1be2fce4378f7ce116124e9
luk3rr <luk3rr@gmail.com>
2023-03-23 13:01:13 -0300

    slightly faster version

--- .config/scripts/r_synp.py
+++ .config/scripts/r_synp.py
@@ -24,64 +24,59 @@ import signal
 import subprocess
 from sys import exit
 from os import getenv
-from threading import Timer
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler, FileClosedEvent, FileOpenedEvent
+from watchdog.events import FileSystemEventHandler
 
 LOCAL_FOLDER = getenv('LOCAL_DOCS_DIR')
 REMOTE_FOLDER = getenv('REMOTE_DOCS_DIR')
 
+# ROOT_FOLDER é o diretório raiz do servidor remoto
+ROOT_FOLDER = REMOTE_FOLDER.split(":")[-1].strip('/')
+
 # Função responsável por sincronizar a pasta local com a remota sempre que o script é inicializado
+# Para diretórios grandes esse trem demora demais
 def pullRemote():
     print(">> SYNC TO REMOTE")
-    cmd = ["rclone", "bisync", str(LOCAL_FOLDER), str(REMOTE_FOLDER), "-vvv", "--resync"]
-    log = subprocess.run(cmd, stdout=subprocess.PIPE)
-    print(log.stdout.decode())
-
-# Função de callback que será chamada sempre que um evento for capturado
-def syncFolder(events):
-    print(">> EVENT DETECTED")
-
-    for event in events:
-        print(event.src_path, event.event_type)
-
-    cmd = ["rclone", "bisync", str(LOCAL_FOLDER), str(REMOTE_FOLDER), "-vvv", "--max-delete", "1000"]
+    cmd = ["rclone", "bisync", str(LOCAL_FOLDER), str(REMOTE_FOLDER), "-vvv", "--resync", "--remove-empty-dirs"]
     log = subprocess.run(cmd, stdout=subprocess.PIPE)
     print(log.stdout.decode())
 
 # Classe responsável por lidar com os eventos de alterações na pasta local
-class my_handler(FileSystemEventHandler):
-    # Construtor da classe
-    def __init__(self, callback):
-        self.events_buffer = []
-        self.callback = callback
-        self.timer = None
-  
-    # Copia a lista de eventos capturados para a variável 'events' e esvazia a lista events_buffer
-    # callback é a função que por fim chama syncFolder e mete bronca
-    def empty_event_buffer(self):
-        events = self.events_buffer.copy()
-        self.events_buffer.clear()
-        self.callback(events)
-    
-    # Override do método on_any_event
-    def on_any_event(self, event):
-        # Ignora os eventos de abertura/fechamento de arquivo ou eventos do tipo diretório
-        # Não quero sincronizar quando for algum deles
-        if event.is_directory or isinstance(event, FileClosedEvent) or isinstance(event, FileOpenedEvent):
+class myHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        if event.is_directory:
             return None
 
-        self.events_buffer.append(event)
+        # A partir do caminho do arquivo alterado, acha o caminho desse arquivo no diretório remoto
+        relativeRemotePath = REMOTE_FOLDER + event.src_path.split(ROOT_FOLDER)[-1].strip('/')
 
-        # Se o timer estiver rodando ele reseta para aguardar mais 10 segundos e capturar mais eventos, caso ocorram
-        # Faço isso para que o rclone não realize uma sincronização por arquivo, ou seja, minimize as  
-        # chamadas no Onedrive (mesmo pagando eles me dão block '-')
-        if self.timer is not None:
-            self.timer.cancel()
+        # Copia o arquivo alterado para o diretório remoto e imprimi a saída do rclone
+        cmd = ["rclone", "copyto", event.src_path, relativeRemotePath, "-vvv"]
+        log = subprocess.run(cmd, stdout=subprocess.PIPE)
+        print(log.stdout.decode())
 
-        # Chama o método empty_event_buffer após 10 seg
-        self.timer = Timer(10.0, self.empty_event_buffer)
-        self.timer.start()
+    def on_modified(self, event):
+        # Criar ou modificar um arquivo merece o mesmo comportamento, então simplesmente chame on_created
+        self.on_created(event)
+
+    def on_deleted(self, event):
+        # A partir do caminho do arquivo deletado, acha o caminho desse arquivo no diretório remoto
+        relativeRemotePath = REMOTE_FOLDER + event.src_path.split(ROOT_FOLDER)[-1].strip('/')
+
+        # Deleta o arquivo no diretório remoto e imprimi a saída do rclone
+        cmd = ["rclone", "delete", relativeRemotePath, "-vvv", "--rmdirs"]
+        log = subprocess.run(cmd, stdout=subprocess.PIPE)
+        print(log.stdout.decode())
+
+    def on_moved(self, event):
+        # A partir do caminho do arquivo, acha o caminho desse arquivo no diretório remoto e para onde ele será movido
+        relativeSrcPath = REMOTE_FOLDER + event.src_path.split(ROOT_FOLDER)[-1].strip('/')
+        relativeDestPath = REMOTE_FOLDER + event.dest_path.split(ROOT_FOLDER)[-1].strip('/')
+
+        # Move o arquivo e imprimi a saída do rclone
+        cmd = ["rclone", "moveto", relativeSrcPath, relativeDestPath, "-vvv"]
+        log = subprocess.run(cmd, stdout=subprocess.PIPE)
+        print(log.stdout.decode())
 
 def main():
     if (LOCAL_FOLDER == None or REMOTE_FOLDER == None):
@@ -90,7 +85,7 @@ def main():
 
     pullRemote()
     
-    event_handler = my_handler(syncFolder)
+    event_handler = myHandler()
     observer = Observer()
 
     observer.schedule(event_handler, path=LOCAL_FOLDER, recursive=True)
